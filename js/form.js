a69cdec008ebef0beb1448f51fc5bd5620c51374
Wilkenson Hilarion <wilkenson@Wilkensons-MBP.home>
2022-01-17 22:08:48 -0500

    renderjs

---
+++ js/form.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('form');
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const status = document.getElementById('status');
+  const data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then(response => {
+      status.innerHTML = ' Successful, Thank you for contacting me!';
+      status.classList.add('success');
+      form.reset();
+    })
+    .catch(error => {
+      status.innerHTML = 'Oops! There was a problem submitting your form';
+      status.classList.add('error');
+    });
+}
+form.addEventListener('submit', handleSubmit);
---
+++ js/render/render.js
@@ -0,0 +1,33 @@
+import { data } from '../data/data.js';
+
+const btnContainer = document.querySelector('.btnContainer');
+const projects = document.querySelector('.allProjects');
+
+renderProjects();
+
+function renderProjects() {
+  data.map(function (data) {
+    const html = `         
+      <div class="mb-1 mx-auto p-height card-project">
+        <a href="${data.URL}" target="_blank">
+          <div class="card mr-1 p-hover" style="width: 18rem;">
+            <img src="img/project${data.id}.png" class="card-img-top" alt="project"/>
+          <div class="card-body ">
+          <p class="card-text text-dark d-flex justify-content-center pb-1">
+            ${data.LANG}          
+          </p>
+          <a href="${data.LINK}" target="_blank" class="btn-c btn btn-primary bg-dark rounded-pill col-6"><i
+            class="bi bi-github"></i>View Code
+          </a>
+          <a href="${data.URL}" target="_blank" 
+                class="btn-c btn btn-primary bg-dark rounded-pill">
+                <i class="bi bi-eye"></i>
+                Live View
+          </a>
+        </a>
+      </div>   
+  `;
+
+    projects.innerHTML += html;
+  });
+}
