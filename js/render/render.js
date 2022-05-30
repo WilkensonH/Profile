733759dee3597504f61963269092d709cd2389c3
wilkenson <tcbwilkenson@gmail.com>
2022-05-30 11:14:43 -0400

    🥳 fabout section glitching and overlaping other section} bug fic

--- js/render/render.js
+++ js/render/render.js
@@ -12,14 +12,11 @@
  function showProjects() {
    works.map(function (work) {
      const html = `         
-      <div class="mb-1 mx-auto p-height card-project">
+      <div class="mx-auto ">
         <a href="${work.url}" target="_blank">
           <div class="card mr-1 p-hover" style="width: 18rem;">
             <img src="img/project${work.id}.png" class="card-img-top" alt="project"/>
-          <div class="card-body ">
-          <p class="card-text text-dark d-flex justify-content-center pb-1">
-            ${work.description}          
-          </p>
+          <div class="card-body ">        
           <a href="${work.link}" target="_blank" class="btn-c btn btn-primary bg-dark rounded-pill col-6"><i
             class="bi bi-github"></i>View Code
           </a>
@@ -28,6 +25,9 @@
                 <i class="bi bi-eye"></i>
                 Live View
           </a>
+          <p class="card-text text-dark d-flex justify-content-center pb-1">
+            ${work.description}          
+          </p>
         </a>
       </div>   
   `;
