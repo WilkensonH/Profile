3dead1521ef95cbabfc4f6ba6974786f283b43c9
wilkenson <tcbwilkenson@gmail.com>
2022-06-10 13:15:12 -0400

    👨🏾‍💻add description to projects

--- js/data/data.js
+++ js/data/data.js
@@ -4,7 +4,10 @@ export const works = [{
     id: 4,
     url: 'https://wilkensonh.github.io/Guessing/', // live view
     link: 'https://github.com/WilkensonH/Guessing.git', // github repo
-    description: '•HTML •CSS •JavaScript',
+    stack: '• HTML • CSS • JavaScript <hr>',
+    description: `Number guessing game, build in vanilla js
+    it tracks the player's score and highest score start playing by entering a
+     number in the box and click check  `,
     lang: "JavaScript",
 
   },
@@ -12,7 +15,10 @@ export const works = [{
     id: 3,
     url: 'https://wilkensonh.github.io/dicegame/', //live view
     link: 'https://github.com/WilkensonH/dicegame.git', //github repo
-    description: '• HTML • CSS • JavaScript',
+    stack: '• HTML • CSS • JavaScript<hr>',
+    description: `This two players dice game is built in vanilla js 
+    it can only be played in one computer. 100 points win the game
+    rolling a 1 cause player to lose all points`,
     lang: "JavaScript",
 
   },
@@ -20,7 +26,9 @@ export const works = [{
     id: 2,
     url: 'https://bit.ly/3DKA81S', //live view
     link: 'https://github.com/WilkensonH/commerce.git', //github repo
-    description: ' •React •stripejs • MUI ',
+    stack: '•React •stripejs • MUI<hr>',
+    description: `Ecom shopping cart, build in react js it's 
+    integrated with commercejs to manage content and stripejs for payment processing`,
     lang: "JavaScript",
 
   },
@@ -28,7 +36,11 @@ export const works = [{
     id: 1,
     url: 'https://wilkensonh.github.io/restaurant_app/', //live view
     link: 'https://github.com/WilkensonH/restaurant_app/', //github repo
-    description: '• HTML • Bootstrap',
+    stack: '• HTML • CSS • JavaScript<hr>',
+    description: `Restaurant website build in vanilla js and bootstrap
+    include custom css some features inculde food showcasing, event booking and stripejs
+    integration   
+    `,
     lang: "JavaScript",
 
   },
--- js/render/render.js
+++ js/render/render.js
@@ -9,21 +9,24 @@ showProjects();
 function showProjects() {
   works.map(function (work) {
     const html = `         
-      <div class="mx-auto">
+      <div class="mb-1 mx-auto">
         <a href="${work.url}" target="_blank">
           <div class="card mr-1 p-hover" style="width: 15rem;">
             <img src="img/project${work.id}.png" class="card-img-top" alt="project"/>
           <div class="card-body ">        
-          <a href="${work.link}" target="_blank" class="btn-c btn btn-primary bg-dark rounded-pill col-6"><i
+          <a href="${work.link}" target="_blank" class="btn-c btn btn-primary bg-dark rounded-pill col-6 mb-1"><i
             class="bi bi-github"></i>Code
           </a>
           <a href="${work.url}" target="_blank" 
-                class="btn-c btn btn-primary bg-dark rounded-pill">
+                class="btn-c btn btn-primary bg-dark rounded-pill mb-1">
                 <i class="bi bi-eye"></i>
                 Live
-          </a>
+          </a>         
           <p class="card-text text-dark d-flex justify-content-center pb-1">
-            ${work.description}          
+            ${work.stack}    
+          </p>
+          <p class="card-text text-dark d-flex justify-content-center pb-1">
+            ${work.description}    
           </p>
         </a>
       </div>   
