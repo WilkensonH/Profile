18f3894bc7e52d1921ae77ad214228a5730f9050
wilkenson <tcbwilkenson@gmail.com>
2022-06-14 23:38:38 -0400

    edit form and add success message

--- js/form.js
+++ js/form.js
@@ -4,19 +4,21 @@ async function handleSubmit(event) {
   event.preventDefault();
   const status = document.getElementById('status');
   const data = new FormData(event.target);
+
   fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      Accept: 'application/json',
-    },
-  })
+      method: form.method,
+      body: data,
+      headers: {
+        Accept: 'application/json',
+      },
+    })
     .then(response => {
-      status.innerHTML = ' Successful, Thank you for contacting me!';
+      status.innerHTML = 'Thank you ! <br>for contacting me!';
       status.classList.add('success');
       form.reset();
     })
     .catch(error => {
+      console.log(error)
       status.innerHTML = 'Oops! There was a problem submitting your form';
       status.classList.add('error');
     });
