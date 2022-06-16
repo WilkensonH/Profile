dfc67c7a023b77bfbac10308bee02d68543e2a41
wilkenson <tcbwilkenson@gmail.com>
2022-06-16 10:24:04 -0400

    update projects

--- js/data/data.js
+++ js/data/data.js
@@ -1,14 +1,13 @@
 // projects and skills informaion
 
 export const works = [{
-    id: 4,
-    url: 'https://wilkensoncode.github.io/Guessing/', // live view
-    link: 'https://github.com/Wilkensoncode/Guessing.git', // github repo
-    stack: '• HTML • CSS • JavaScript <hr>',
-    description: `Number guessing game, build in vanilla js
-    it tracks the player's score and highest score start playing by entering a
-     number in the box and click check  
-     `,
+    id: 2,
+    url: 'https://bit.ly/3DKA81S', //live view
+    link: 'https://github.com/Wilkensoncode/commerce.git', //github repo
+    stack: '•React •stripejs • MUI<hr>',
+    description: `Ecom shopping cart, build in react js it's 
+    integrated with commercejs to manage content and stripejs for payment processing
+    `,
     lang: "JavaScript",
 
   },
@@ -25,26 +24,28 @@ export const works = [{
 
   },
   {
-    id: 2,
-    url: 'https://bit.ly/3DKA81S', //live view
-    link: 'https://github.com/Wilkensoncode/commerce.git', //github repo
-    stack: '•React •stripejs • MUI<hr>',
-    description: `Ecom shopping cart, build in react js it's 
-    integrated with commercejs to manage content and stripejs for payment processing
-    `,
+    id: 4,
+    url: 'https://wilkensoncode.github.io/Guessing/', // live view
+    link: 'https://github.com/Wilkensoncode/Guessing.git', // github repo
+    stack: '• HTML • CSS • JavaScript <hr>',
+    description: `Number guessing game, build in vanilla js
+    it tracks the player's score and highest score start playing by entering a
+     number in the box and click check  
+     `,
     lang: "JavaScript",
 
   },
-  {
-    id: 1,
-    url: 'https://wilkensoncode.github.io/restaurant_app/', //live view
-    link: 'https://github.com/Wilkensoncode/restaurant_app/', //github repo
-    stack: '• HTML • CSS • JavaScript<hr>',
-    description: `Restaurant website build in vanilla js and bootstrap
-    include custom css some features inculde food showcasing, event booking and stripejs
-    integration   
-    `,
-    lang: "JavaScript",
 
-  },
+  // {
+  //   id: 1,
+  //   url: 'https://wilkensoncode.github.io/restaurant_app/', //live view
+  //   link: 'https://github.com/Wilkensoncode/restaurant_app/', //github repo
+  //   stack: '• HTML • CSS • JavaScript<hr>',
+  //   description: `Restaurant website build in vanilla js and bootstrap
+  //   include custom css some features inculde food showcasing, event booking and stripejs
+  //   integration   
+  //   `,
+  //   lang: "JavaScript",
+
+  // },
 ];
