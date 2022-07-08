938b737c7fa31351d224160470db2e3ca7a963b9
wilkenson <tcbwilkenson@gmail.com>
2022-07-08 12:24:33 -0400

    css animation/ js

--- js/script/main.js
+++ js/script/main.js
@@ -1,5 +1,4 @@
 // smooth scroll  
-
 $('.navbar a , .link a').on('click', function (e) {
     if (this.hash !== "") {
         e.preventDefault();
@@ -16,8 +15,38 @@ $(function () {
         if ($(window).scrollTop() > 60) {
             $(".header").addClass("header-styles");
         } else {
-            //remove the background property so it comes transparent again (defined in your css)
             $(".header").removeClass("header-styles");
         }
     });
-});
+})
+
+// about sec/title slide right
+$(function () {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 200) {
+            $(".about-slide-rt").addClass("slide-right");
+        } else {
+            $(".about-slide-rt").removeClass("slide-right");
+        }
+    });
+})
+//slide about description left
+$(function () {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 300) {
+            $(".about-slide-lf").addClass("slide-left");
+        } else {
+            $(".about-slide-lf").removeClass("slide-left");
+        }
+    });
+})
+//rotating code snippet
+$(function () {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 500) {
+            $(".about-code-snip").addClass("rotate");
+        } else {
+            $(".about-code-snip").removeClass("rotate");
+        }
+    });
+})
